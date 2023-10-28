5a26607e50c599463c7afb560618260360dd9dbb
ilknurrkartop <ilknurrkartop@gmail.com>
2023-10-29 01:51:46 +0300

    .

--- Work - Bonus/main.py
+++ Work - Bonus/main.py
@@ -1,8 +1,15 @@
 baslik  ="HABERİNİZ OLSUN"  #string
 vade = 12  #integer
 faizOrani = 1.47 #float
+faizOrani1 = 1.44
+faiz_orani = 1.47
 
 print(baslik)
 print(type(baslik))
 print(type(vade))
 print(type(faizOrani))
+
+mesaj = "Hoşgeldin"
+musteriAdi= "İlknur"
+musteriSoyadi = "Kartop"
+print(mesaj + musteriAdi + musteriSoyadi)
