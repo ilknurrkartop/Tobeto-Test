90d7f7fb278a140aaea3caf9cb916b95356aab63
ilknurrkartop <ilknurrkartop@gmail.com>
2023-10-30 00:25:41 +0300

    .

---
+++ Work - Bonus/sartBloklari.py
@@ -0,0 +1,23 @@
+dolarDun = 7.85
+dolarBugun = 7.85
+
+if dolarDun > dolarBugun:
+    print("Azalış oku")
+    print("Bitti")
+elif dolarDun < dolarBugun:
+    print("Artış oku")
+else:
+    print("Eşittir oku")
+
+print("Bitti")
+
+
+
+# if dolarDun < dolarBugun:
+#     print("Artış oku")
+
+# if dolarDun == dolarBugun:
+#     print("Eşittir oku")
+
+
+
