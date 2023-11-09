eb3a1345ac904da1317e2209fb7c72a385ad31a4
ilknurrkartop <ilknurrkartop@gmail.com>
2023-11-09 13:28:42 +0300

    Update 3.sql

--- PostegroSQL/3.sql
+++ PostegroSQL/3.sql
@@ -5,3 +5,11 @@ Select company_name from suppliers
 where EXISTS(Select product_name from products
 			where suppliers.supplier_id=products.supplier_id
 			AND unit_price<20)
+			
+Select company_name from suppliers
+where NOT EXISTS(Select product_name from products
+			where suppliers.supplier_id=products.supplier_id
+			AND unit_price=20)
+
+--'3/5/1998' ile '4/5/1998' tarihleri arasında sipariş almış çalışanları listeliyoruz.
+
