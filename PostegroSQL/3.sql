33454a2080d5b7a43bc52280ce8ae8402e2824cd
ilknurrkartop <ilknurrkartop@gmail.com>
2023-11-09 13:35:42 +0300

    Update 3.sql

--- PostegroSQL/3.sql
+++ PostegroSQL/3.sql
@@ -13,3 +13,7 @@ where NOT EXISTS(Select product_name from products
 
 --'3/5/1998' ile '4/5/1998' tarihleri arasında sipariş almış çalışanları listeliyoruz.
 
+Select e.employee_id,e.first_name,e.last_name
+from employees e
+inner join orders o ON e.employee_id = o.employee_id
+where o.order_date BETWEEN DATE '1998-05-03' AND DATE '1998-05-04'
