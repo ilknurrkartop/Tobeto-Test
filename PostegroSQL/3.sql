8be20a21683677190d2182ab7d67754a5f5d497f
ilknurrkartop <ilknurrkartop@gmail.com>
2023-11-09 13:42:31 +0300

    Update 3.sql

--- PostegroSQL/3.sql
+++ PostegroSQL/3.sql
@@ -12,8 +12,17 @@ where NOT EXISTS(Select product_name from products
 			AND unit_price=20)
 
 --'3/5/1998' ile '4/5/1998' tarihleri arasında sipariş almış çalışanları listeliyoruz.
-
+Select * from employees e
+where exists (Select * from orders o
+			where o.employee_id = e.employee_id
+			AND o.order_date BETWEEN DATE '3/5/1998' AND DATE '4/5/1998')
+			
+--2.çözüm			
 Select e.employee_id,e.first_name,e.last_name
 from employees e
 inner join orders o ON e.employee_id = o.employee_id
 where o.order_date BETWEEN DATE '1998-05-03' AND DATE '1998-05-04'
+
+
+
+			
