38f6d59c0416c576a4293eff32be7397ee99ec37
ilknurrkartop <ilknurrkartop@gmail.com>
2023-12-21 14:58:12 +0300

    js

---
+++ Seleniumstart/Test_DemoClass.py
@@ -0,0 +1,3 @@
+class Test_DemoClass:
+    def test_demo(self):
+        print("x")
--- Seleniumstart/Test_SonucDemo.py
+++ Seleniumstart/Test_SonucDemo.py
@@ -1,30 +1,53 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager . chrome import ChromeDriverManager
 from time import sleep
-from selenium.webdriver.support.wait import WebDriverWait #ilgili driverı bekleten yapı
-from selenium.webdriver.support import expected_conditions #beklenen koşullar
+from selenium.webdriver.support.wait import WebDriverWait #beklediğim koşulu sağlayana kadar bekle, ilgili driverı bekleten yapı
+from selenium.webdriver.support import expected_conditions as ec #beklenen koşullar
 from selenium.webdriver.common.action_chains import ActionChains
 
-class Test_SonucDemo:
-    def test_invalid_login():
-        driver = webdriver.Chrome()
-        driver.get("https://www.google.com")
-        driver.maximize_window() #ekranı büyütür
-        
-        usernameInput = driver.find_element(By.ID, "user-name")
+class Test_SauceDemo:
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get("https://www.saucedemo.com")
+        self.driver.maximize_window() 
+                
+    def test_invalid_login(self):
+        self.driver.get("https://www.saucedemo.com")
+        usernameInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID , "user-name")))
         usernameInput.send_keys("locked_out_user")
+      
+        passwordInput =WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID , "password")))
+        passwordInput.send_keys("secret_sauce")
+      
+        loginButton = self.driver.find_element(By.ID , "login-button")
+        loginButton.click()
+     
+        errorMessage = self.driver.find_element(By.XPATH, "//*[@id='login_button_container']/div/form/div[3]/h3")
+        testResult = errorMessage.text == "Epic sadface: Sorry, this user has been locked out."
+        print(f"TEST SONUCU: {testResult}")
         
-        passwordInput = driver.find_element(By.ID, "password")
+    def valid_login(self):
+        self.driver.get("https://www.saucedemo.com")
+        usernameInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID , "user-name")))
+        usernameInput.send_keys("standard_user")
+      
+        passwordInput =WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID , "password")))
         passwordInput.send_keys("secret_sauce")
         
-        loginButton = driver.find_element(By.ID, "login-button")
+        loginButton = self.driver.find_element(By.ID , "login-button")
         loginButton.click()
         
-        errorMessage = driver.find_element(By.XPATH, "//*[@id='rso']/div[1]/div/div/div/div/div/div/div/div[1]/div/span/a/h3")
-        testResult  = errorMessage.text("Epic ")
-        print(f"TEST SONUCU: {testResult}")
+        #headerLogo = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.XPATH , "//*[@id='header_container']/div[1]/div[2]/div")))
+        #testResult = headerLogo.text == "Swag Labs"
+        self.driver.execute_script("window.scrollTo(0,500)") #js scrooll ile kaydırma yapılır. 
+        addToCart = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By . XPATH , "//*[@id='add-to-cart-test.allthethings()-t-shirt-(red)']")))
+        addToCart.click()
+        remove = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By . XPATH , "//*[@id='remove-test.allthethings()-t-shirt-(red)']")))
+        testResult = remove.text == "Remove"
+        print(f"Test Sonucu: {testResult}")
+        sleep(3)
 
-
-testClass = Test_SonucDemo()
+testClass = Test_SauceDemo()
 testClass.test_invalid_login()
+testClass.valid_login()
